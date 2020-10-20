03827bfd8bd303b5fc23f8b04fe4f49e40d23989
Michael Wetterauer <mjw0486@gmail.com>
2020-10-19 20:30:58 -0400

    Connected VideoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,28 @@
 import Search from './Search.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
-
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer />
-      </div>
-      <div className="col-md-5">
-        <VideoList />
+const App = () => {
+  console.log(exampleVideoData);
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search />
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer />
+        </div>
+        <div className="col-md-5">
+          <VideoList />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
